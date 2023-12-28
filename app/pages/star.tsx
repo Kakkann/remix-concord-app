8e19b3437f00a9305fd61c00ba1399ed3e80c5b3
Kakkann <kaneki.adictter@gmail.com>
2023-12-27 22:09:26 -0300

    Div Key
    Added a key to each star div

--- app/pages/star.tsx
+++ app/pages/star.tsx
@@ -2,7 +2,7 @@ export default function Star() {
   const star: JSX.Element[] = [];
 
   for (let i = 0; i < 50; i++) {
-    star[i] = <div className="star"></div>;
+    star[i] = <div key={i} className="star"></div>;
   }
 
   return star;
